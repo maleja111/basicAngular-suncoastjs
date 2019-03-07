b811665bec4ed0cff0da7879e044d8a8f16601d4
Alejandra Giraldo <m.aleja3000@gmail.com>
2019-03-07 00:20:33 -0500

    Add Menu Component-mAdd Footer Component
    Add User Form Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormUsersComponent } from './form-users/form-users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    FormUsersComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/form-users/form-users.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-users',
+  templateUrl: './form-users.component.html',
+  styleUrls: ['./form-users.component.scss']
+})
+export class FormUsersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
